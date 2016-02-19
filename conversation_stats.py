6989ea8295b5eebfc03696a19de24522d4c53f04
L3viathan <git@l3vi.de>
2016-02-19 22:10:01 +0100

    average length of conversation per book

--- conversation_stats.py
+++ conversation_stats.py
@@ -1,8 +1,22 @@
 import json
 from collections import Counter
 
-with open("conversations.jsonl") as f:
-    lengths = Counter(len(json.loads(line)['conversation']) for line in f)
+booksum, booknum = Counter(), Counter()
+bookmeans = Counter()
+lengths = Counter()
 
+with open("conversations.jsonl") as f:
+    for line in f:
+        chain = json.loads(line)
+        booksum[chain['book_title']] += len(chain['conversation'])
+        booknum[chain['book_title']] += 1
+        lengths[len(chain['conversation'])] += 1
+
+for book in booksum:
+    bookmeans[book] = booksum[book]/booknum[book]
+
+# Average length of conversation by book:
+print(bookmeans)
+
+# Frequency of lengthy conversations overall (Zipf-y)
 print(lengths)
-print(lengths.keys(), lengths.values())
